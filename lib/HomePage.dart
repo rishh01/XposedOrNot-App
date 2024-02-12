edc30d01e33dd68a15a60f0d3182358dfd300534
rishh01 <rishabhsuryavanshi2004@gmail.com>
2024-02-12 22:11:49 +0530

    Frontend complete

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -24,41 +24,49 @@ class _HomePageState extends State<HomePage> {
           fontSize: 25,
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _textcontroller,
-              decoration: const InputDecoration(
-                hintText: "Enter email to check",
-                border: OutlineInputBorder(),
+      body: Container(
+          color: const Color(0xff181B2D),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Check if Your Email Address is in a Data Breach:',
+              style: TextStyle(fontFamily: 'NeometricAltBlack',
+              color: Color(0xff9FC0E0),
+              fontSize: 20),),
+              TextField(
+                controller: _textcontroller,
+                decoration: const InputDecoration(
+                  hintText: "Enter email to check",
+                  hintStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder(),
+                ),
               ),
-            ),
-            MaterialButton(
-              onPressed: () {},
-              color: Colors.green,
-              child: const Text(
-                "Check",
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            const SizedBox(height: 50),
-            Link(
-              uri: websiteUri,
-              target: LinkTarget.defaultTarget,
-              builder: (context, openLink) => TextButton(
-                onPressed: openLink,
+              MaterialButton(
+                onPressed: () {},
+                color: Colors.green,
                 child: const Text(
-                  'Data Breach - Next Steps',
-                  style: TextStyle(
-                    decoration: TextDecoration.underline,
+                  "Check",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 50),
+              Link(
+                uri: websiteUri,
+                target: LinkTarget.defaultTarget,
+                builder: (context, openLink) => TextButton(
+                  onPressed: openLink,
+                  child: const Text(
+                    'Data Breach - Next Steps',
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
